8b1c083433164262c307a3238250451fb9eb8213
100798f <farhodbek.nomonov@mail.ru>
2022-03-28 15:49:38 +0500

    fixed

---
+++ src/assets/fake-data/data-slider.js
@@ -0,0 +1,38 @@
+import img1 from '../images/slider/slide_1.png';
+import img2 from '../images/slider/slide_1.png';
+import imgbg1 from '../images/slider/bg_slide_1.png'
+import imgbg2 from '../images/slider/bg_slide_1.png'
+
+
+const heroSliderData = [
+    {
+        title_1: "Discover/find/create",
+        title_2: "Sell instantly",
+        title_3: "The impressive NFTs",
+        description: "The first NFT-Project based on NEAR-Ptotocol",
+        img: img1,
+        imgbg: imgbg1,
+        class:'left'
+    },
+    {
+        title_1: "Discover/find/create",
+        title_2: "Sell instantly",
+        title_3: "The impressive NFTs",
+        description: "The first NFT-Project based on NEAR-Ptotocol",
+        img: img2,
+        imgbg: imgbg2,
+        class:'center'
+    },
+    {
+        title_1: "Discover/find/create",
+        title_2: "Sell instantly",
+        title_3: "The impressive NFTs",
+        description: "The first NFT-Project based on NEAR-Ptotocol",
+        img: img2,
+        imgbg: imgbg2,
+        class:'right'
+    },
+    
+]
+
+export default heroSliderData;
---
+++ src/pages/WalletConnect.jsx
@@ -0,0 +1,77 @@
+import React , { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+
+import img8 from '../assets/images/icon/connect-8.png'
+
+const WalletConnect = () => {
+    const [data] = useState(
+        [
+            {
+                img: img8,
+                title: 'NEAR-protocol',
+                description: ' Incredibly fast'
+               
+            },
+           
+            
+        ]
+    )
+    return (
+        <div>
+            <Header />
+            <section className="flat-title-page inner">
+                <div className="overlay"></div>
+                <div className="themesflat-container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="page-title-heading mg-bt-12">
+                                <h1 className="heading text-center">Connect Wallet</h1>
+                            </div>
+                            <div className="breadcrumbs style2">
+                                <ul>
+                                    <li><Link to="/">Home</Link></li>
+                                    <li><Link to="#">Pages</Link></li>
+                                    <li>Connect Wallet</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>                    
+            </section>
+            <div className="tf-connect-wallet tf-section">
+                <div className="themesflat-container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h2 className="tf-title-heading ct style-2 mg-bt-12">
+                                Connect Your Wallet
+                            </h2>
+                            <h5 className="sub-title ct style-1 pad-400">
+                            Connect using a Near Protocol wallet to easily use the marketplace.
+                            </h5>
+                        </div>
+                        <div className="col-md-12">
+                            <div className="sc-box-icon-inner style-2">
+                                {
+                                    data.map((item,index) => (
+                                        <div key={index} className="sc-box-icon">
+                                            <div className="img">
+                                                <img src={item.img} alt="Axies" />
+                                            </div>
+                                            <h4 className="heading"><Link to="/login">{item.title}</Link> </h4>
+                                            <p className="content">{item.description}</p>
+                                         </div>
+                                    ))
+                                }
+                            </div>  
+                        </div>    
+                    </div>              
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default WalletConnect;
