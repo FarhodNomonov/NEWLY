5024ca772b5dff4f9b71ff86a5a618e74ccd9743
1xteam <100232914+1xteam@users.noreply.github.com>
2022-03-28 14:27:26 +0300

    komentariy

--- src/components/layouts/explore-01/TodayPicks.jsx
+++ src/components/layouts/explore-01/TodayPicks.jsx
@@ -20,51 +20,23 @@ const TodayPicks = props => {
                     <div className="row">
                         <div className="col-md-12">
                             <div className="wrap-box explore-1 flex mg-bt-40">
-                                <div className="seclect-box style-1">
-                                    <div id="item_category" className="dropdown">
-                                        <Link to="#" className="btn-selector nolink">All categories</Link>
-                                        <ul >
-                                            <li><span>Art</span></li>
-                                            <li className="active"><span>Music</span></li>
-                                            <li><span>Domain Names</span></li>
-                                            <li><span>Virtual World</span></li>
-                                            <li><span>Trading Cards</span></li>
-                                            <li><span>Sports</span></li>
-                                            <li><span>Utility</span></li>
-                                        </ul>
-                                    </div>
-                                    <div id="buy" className="dropdown">
-                                        <Link to="#" className="btn-selector nolink">Buy Now</Link>
-                                        <ul >
-                                            <li><span>On Auction</span></li>
-                                            <li><span>Has Offers</span></li>
-                                        </ul>
-                                    </div>
-                                    <div id="all-items" className="dropdown">
-                                        <Link to="#" className="btn-selector nolink">All Items</Link>
-                                        <ul >
-                                            <li><span>Single Items</span></li>
-                                            <li><span>Bundles</span></li>
-                                        </ul>
-                                    </div>
-                                </div>
+                        
                                 <div className="seclect-box style-2 box-right">
                                     <div id="artworks" className="dropdown">
-                                        <Link to="#" className="btn-selector nolink">All Artworks</Link>
+                                        <Link to="#" className="btn-selector nolink">Addition time</Link>
                                         <ul >
-                                            <li><span>Abstraction</span></li>
-                                            <li><span>Skecthify</span></li>
-                                            <li><span>Patternlicious</span></li>
-                                            <li><span>Virtuland</span></li>
-                                            <li><span>Papercut</span></li>
+                                            <li><span>Today</span></li>
+                                            <li><span>Last 7 days</span></li>
+                                            <li><span>Last month</span></li>
+                                            <li><span>All Time</span></li>
                                         </ul>
                                     </div>
                                     <div id="sort-by" className="dropdown">
                                         <Link to="#" className="btn-selector nolink">Sort by</Link>
                                         <ul >
-                                            <li><span>Top rate</span></li>
-                                            <li><span>Mid rate</span></li>
-                                            <li><span>Low rate</span></li>
+                                            <li><span>Top Rate</span></li>
+                                            <li><span>Top Sales</span></li>
+                                            <li><span>Top Views</span></li>
                                         </ul>
                                     </div>    
                                 </div>
